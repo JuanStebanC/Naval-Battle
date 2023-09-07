25c6ba3a7eb42074218e9fe5388393c33c6f1bd8
JuanStebanC <juanstebanleoncardenas@gmail.com>
2023-09-07 12:25:58 -0500

    Creación del proyecto

---
+++ src/edu/uptc/view/Runner.java
@@ -0,0 +1,15 @@
+package edu.uptc.view;
+
+import javax.swing.*;
+
+public class Runner {
+    //método main
+    public static void main(String[] args) {
+    }
+
+    //método menú
+    private static void mainMenu() {
+        String menuArray[] = { "Jugar"};
+        String menu = JOptionPane.showInputDialog();
+    }
+}
